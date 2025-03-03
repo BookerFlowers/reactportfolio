51dc2fd0ea5a99a26408f0f1e56eac06253713ed
BookerFlowers <flowersbooker2025@gmail.com>
2025-03-03 14:36:15 -0500

    1: 2 containers, style with flex, align item center, justify content center
    2: first container (1 - h1, 1 - h2, 1 - p) Put Name as header, title below that, your icons, the brief description of yourself, style container with flex, flex direction column, justify content center, gap
    
    Open Screenshot 2025-03-03 133406.png
    Screenshot 2025-03-03 133406.png
      30px
    
    3: Import LinkedIn & git hub icons, give them onclick with a function to navigate to corresponding pages. give icons transition scale to 1.2, and color change when hovered over.
    
    4: second container import image of yourself

--- reactportfolio/src/Pages/Home.jsx
+++ reactportfolio/src/Pages/Home.jsx
@@ -10,12 +10,12 @@ const Home = () => {
                 <h1>Booker T. Flowers </h1>
                 <h2>Soft Developer  </h2>
                 <p>Booker is a graduate of Persevere where he earned his full stack certificate on December 13, 2025. He is currently an intern software developer with Banyan Labs. Currently enrolled in Grand Canyon University to take online classes to obtain a bachelors of arts degree in Web Design.  </p>
-<div className='flex justify-around '>
-           <button className='transition-scale-1.2 transition-colors hover:bg-red-500' onClick={() => window.location.href = 'https://www.linkedin.com/feed/'}><img src="src\assets\images\LinkedInlink.png" alt="linkedIn" /></button >
-                <button className='transition-scale-1.2 transition-colors hover:bg-red-500' onClick={() => window.location.href = 'https://github.com/BookerFlowers'}><img src="src\assets\images\GitHublink.png" alt="GitHub" /></button>
-</div>
+                <div className='flex justify-around '>
+                    <button className='transition-scale-1.2 transition-colors hover:bg-red-500' onClick={() => window.location.href = 'https://www.linkedin.com/feed/'}><img src="src\assets\images\LinkedInlink.png" alt="linkedIn" /></button >
+                    <button className='transition-scale-1.2 transition-colors hover:bg-red-500' onClick={() => window.location.href = 'https://github.com/BookerFlowers'}><img src="src\assets\images\GitHublink.png" alt="GitHub" /></button>
+                </div>
 
-         
+
 
 
 
@@ -24,9 +24,9 @@ const Home = () => {
             </div>
             <div className='flex items-center justify-center w-full h-full ' >
                 <div className='max-h-max max-w-max '>
-                      <img src="src\assets\images\pic14.png" alt="profilePic" />
+                    <img src="src\assets\images\pic14.png" alt="profilePic" />
                 </div>
-              
+
             </div>
 
 
--- reactportfolio/src/layouts/Rootlayout.jsx
+++ reactportfolio/src/layouts/Rootlayout.jsx
@@ -14,11 +14,11 @@ function RootLayout() {
                 <nav className='bg-black text-blue-300 aspect-auto'>
                    
                     <span className='flex items-center justify-center sm:gap-0.5 md:gap-10 lg:gap-20 xl:gap-36 2xl:gap-40'>
-                        <img className='w-8 gap-10 text-9*1 font-extrabold ' src='src/assets/images/pic14.png' alt='logo' />
+                        <img className='w-8 gap-10 text-9*1 font-extrabold rounded-full' src='src/assets/images/pic14.png' alt='logo' />
                         <NavLink to='/'>Home</NavLink>
                         <NavLink to='Projects'>Projects</NavLink>
                         <NavLink to='Skills'>Skills</NavLink>
-                        <img className='w-8 gap-10 text-9*1 font-extrabold ' src='src/assets/images/pic14.png' alt='logo' />
+                        <img className='w-8 gap-10 text-9*1 font-extrabold rounded-full' src='src/assets/images/pic14.png' alt='logo' />
                     </span>
 
                 </nav>
